e63b753561ede4c2b9a6cd8c85a3a43f5caa2807
GOATst3 <stefano2301@live.it>
2022-09-02 15:35:13 +0200

    Es18 Retrieve, update and delete a resource

--- Es15/server.js
+++ Es15/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { planet } = require('./client');
 const prisma = require('./client');
 require('dotenv').config();
 
@@ -9,16 +8,33 @@ app.use(express.json())
 
 const port=process.env.PORT
 
-app.get('/',(req,res)=>{
-    res.json({res:'this is a response'})
-})
 
+
+
+//find all planets
 app.get('/planets', async (req,res)=>{
     const planets = await prisma.Planet.findMany()
     res.json(planets)
 })
 
-app.post('/planets', async (req, res) => {
+
+//find planet by id  // the \\d+ is a regex that stands for "only digits [0-9], composed by one or more character"
+app.get('/planet/:id(\\d+)', async (req,res,next)=>{
+    const planetID = Number(req.params.id)
+
+    const planet = await prisma.Planet.findUnique({
+        where:{id:planetID}
+    })
+    if(!planet){
+        res.status(404)
+        return next('Cannot GET /planet/'+planetID)
+    }
+    res.json(planet)
+})
+
+
+//add a planet into db 
+app.post('/planet', async (req, res) => {
     const data = req.body
     const planet = await prisma.planet.create({
         data : data
@@ -27,6 +43,41 @@ app.post('/planets', async (req, res) => {
     res.status(201).json(planet);
 })
 
+//update a planet
+app.put('/planet/:id(\\d+)', async (req, res, next) => {
+    const data = req.body
+    const planetID = Number(req.params.id)
+
+    try{
+        const planet = await prisma.planet.update({
+            where:{id : planetID},
+            data : data
+        });
+        res.status(200).json(planet);
+    }
+
+    catch (e) {
+        res.status(404)
+        return next('Cannot Update /planet/'+planetID)
+    }
+})
+
+//delete a planet
+app.delete('/planet/:id(\\d+)', async (req, res, next) => {
+    const planetID = Number(req.params.id)
+
+    try{
+        await prisma.planet.delete({
+            where:{id : planetID},
+        });
+        res.status(204).end();
+    }
+
+    catch (e) {
+        res.status(404)
+        return next('Cannot delete /planet/'+planetID)
+    }
+})
 
 app.listen(port,()=>console.log('Server is running on localhost:'+port))
 
